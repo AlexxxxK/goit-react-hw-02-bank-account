b752fd942b38d14faacc045181f88d4e10ce8d9f
Oleg Kozyr <olegvkozyr@gmail.com>
2019-09-24 02:16:12 +0300

    Updates

---
+++ precache-manifest.adaf1056fe16e304ef6f3fb81dfb5c2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696db45434bfb42d9fccc70a9772b088",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "ae327fccf365a8a05654",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "4e88f10ff49aba42c32e",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.f41b2d19.chunk.css"
+  },
+  {
+    "revision": "ae327fccf365a8a05654",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.4713c1f5.chunk.js"
+  },
+  {
+    "revision": "4e88f10ff49aba42c32e",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.184e1015.chunk.js"
+  },
+  {
+    "revision": "05b2ae0eff8becfb8aa8",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime~main.44da4654.js"
+  }
+]);
