50186e7adc8f0ad4fd35b7e2db3774864caf91c6
AlexxxxK <oleksandr.kozyr@outlook.com>
2019-10-07 02:16:11 +0300

    Updates

--- precache-manifest.30ffffdecdeeb73d295cfd46feb17303.js
+++ precache-manifest.30ffffdecdeeb73d295cfd46feb17303.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ba2be4ca9517e710cf973a3b20d5ecee",
+    "revision": "80a41a2d70159380839ef3b111e3e67a",
     "url": "/goit-react-hw-02-bank-account/index.html"
   },
   {
-    "revision": "3e13ac652e1dadbc1786",
+    "revision": "853931109f239a2b9563",
     "url": "/goit-react-hw-02-bank-account/static/css/2.39017165.chunk.css"
   },
   {
-    "revision": "040eab47150ccb5fa24c",
+    "revision": "1784b4e94f653a349721",
     "url": "/goit-react-hw-02-bank-account/static/css/main.feeb0e5a.chunk.css"
   },
   {
-    "revision": "3e13ac652e1dadbc1786",
-    "url": "/goit-react-hw-02-bank-account/static/js/2.2605c94a.chunk.js"
+    "revision": "853931109f239a2b9563",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.8a7e4b35.chunk.js"
   },
   {
-    "revision": "040eab47150ccb5fa24c",
-    "url": "/goit-react-hw-02-bank-account/static/js/main.64a92521.chunk.js"
+    "revision": "1784b4e94f653a349721",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.657ca7be.chunk.js"
   },
   {
     "revision": "05b2ae0eff8becfb8aa8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-bank-account/precache-manifest.7b10cba629dc447a4d2bc0eea024abe2.js"
+  "/goit-react-hw-02-bank-account/precache-manifest.30ffffdecdeeb73d295cfd46feb17303.js"
 );
 
 self.addEventListener('message', (event) => {
